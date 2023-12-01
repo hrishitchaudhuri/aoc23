b3cf34bb0be44980cc19df01ab5a14b03c7e550d
Hrishit Chaudhuri <57035481+hrishitchaudhuri@users.noreply.github.com>
2023-12-01 11:40:47 -0500

    Update day1.ml

--- day1.ml
+++ day1.ml
@@ -9,19 +9,31 @@ let read_file filename =
         close_in chan;
         List.rev !lines
 
-let sum_l (l) = List.fold_left (fun x y -> x + y) 0 l
+let get_int_str = function
+  | "zero" -> "0"
+  | "one" -> "1"
+  | "two" -> "2"
+  | "three" -> "3"
+  | "four" -> "4"
+  | "five" -> "5"
+  | "six" -> "6"
+  | "seven" -> "7"
+  | "eight" -> "8"
+  | "nine" -> "9"
+  | x -> x
+
+let sum_l (l) = List.fold_left (fun x y -> x + y) (int_of_string "0") l
 
 let search_first (x) = 
-  let pos = Str.search_forward (Str.regexp "[0-9]") x 0 in
-  String.sub x pos 1 
+  let _ = Str.search_forward (Str.regexp {|[0-9]\|one\|two\|three\|four\|five\|six\|seven\|eight\|nine\|zero|}) x (int_of_string "0")  in
+  Str.matched_string x 
 
 let search_last (x) =
-  let pos = Str.search_backward (Str.regexp "[0-9]") x (String.length x) in
-  String.sub x pos 1
+  let _ = Str.search_backward (Str.regexp {|[0-9]\|one\|two\|three\|four\|five\|six\|seven\|eight\|nine\|zero|}) x (String.length x) in
+  Str.matched_string x
 
 let final = read_file "/home/hrisc/Documents/aoc/inputs/day1"
-              |> List.map (fun x -> int_of_string (String.concat "" [search_first x ; search_last x]))
+              |> List.map (fun x -> int_of_string (String.concat "" [ get_int_str (search_first x) ; get_int_str (search_last x)]))
               |> sum_l
       
 let () = print_int final; print_endline "";;
-
